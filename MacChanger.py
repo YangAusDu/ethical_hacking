d135434588ccf3d6acf6557ce1426ac1b46f5e5a
root <root@localhost.localdomain>
2019-10-15 03:05:56 -0400

    macchanger list

--- MacChanger.py
+++ MacChanger.py
@@ -6,7 +6,13 @@ interface = input('Change mac address for: ')
 new_mac_addr = input('type your new mac address: ')
 print(new_mac_addr)
 
-subprocess.call('ifconfig '+ str(interface) + ' down', shell = True)
-subprocess.call('ifconfig '+str(interface)+ ' hw ether 01:5d:6c:e8:8d:b2', shell = True)
-subprocess.call('ifconfig '+ str(interface) + ' up', shell = True)
+#subprocess.call('ifconfig '+ str(interface) + ' down', shell = True)
+#subprocess.call('ifconfig '+str(interface)+ ' hw ether 01:5d:6c:e8:8d:b2', shell = True)
+#subprocess.call('ifconfig '+ str(interface) + ' up', shell = True)
+#subprocess.call('ifconfig', shell = True)
+
+subprocess.call(['ifconfig', str(interface), 'down'])
+subprocess.call(['ifconfig', str(interface), 'hw', 'ether', new_mac_addr])
+subprocess.call(['ifconfig', str(interface), 'up'])
 subprocess.call('ifconfig', shell = True)
+
